bbeeb0e3712f46c079866a4d9a2591609acca1dc
selfcac <selfcac@5gbinbox.com>
2019-09-13 06:32:15 +0300

    NamedPipesHelper + SID security

---
+++ NamedPipesHelper.Example/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NamedPipesHelper;
+
+namespace NamedPipesHelper.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!args.Contains("--no-server"))
+            {
+                Server myServer = new Server((t) => Console.WriteLine(t), (t) => Console.WriteLine(t));
+                myServer.Start();
+            }
+
+            Console.WriteLine(Client.getSecret());
+            Console.WriteLine(Client.getSecret());
+            Console.WriteLine(Client.getSecret());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ NamedPipesHelper/Client.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamedPipesHelper
+{
+    public class Client
+    {
+        public static string getSecret()
+        {
+            string result = "secret-init";
+
+            using (var client = new NamedPipeClientStream(".", Server.pipeName, PipeDirection.In))
+            {
+                client.Connect();
+
+                using (var reader = new StreamReader(client))
+                {
+                    result = reader.ReadLine();
+                }
+
+            }
+
+            return result;
+        }
+    }
+}
